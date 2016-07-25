8702afb2676e490f93c65742f2a22361bb908cd5
Anton Shkurenko <tonyshkurenko@gmail.com>
2016-07-25 19:22:30 +0300

    Update project

--- app/src/main/java/cullycross/clock/Clock.java
+++ app/src/main/java/cullycross/clock/Clock.java
@@ -8,20 +8,14 @@ import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
+public class Clock extends Activity {
 
-public class Clock extends Activity{
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clock);
-        TextView vk = (TextView) findViewById(R.id.textView2);
-        vk.setMovementMethod(LinkMovementMethod.getInstance());
-
-        Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
-        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
-                new ComponentName(this, ValtoneClock.class));
-        startActivity(intent);
-
-    }
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_clock);
+    Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+    intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+        new ComponentName(this, ValtoneClock.class));
+    startActivity(intent);
+  }
 }
--- app/src/main/java/cullycross/clock/ValtoneClock.java
+++ app/src/main/java/cullycross/clock/ValtoneClock.java
@@ -6,96 +6,101 @@ import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.os.Handler;
 import android.service.wallpaper.WallpaperService;
-import android.util.Log;
 import android.view.SurfaceHolder;
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-
+import java.util.Locale;
 
 public class ValtoneClock extends WallpaperService {
+
+  private static final DateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss", Locale.US);
+  private static final int DELAY_MILLIS = 1000;
+  private static final Paint PAINT = new Paint();
+
+  static {
+    PAINT.setColor(Color.WHITE);
+    PAINT.setAntiAlias(true);
+  }
+
+  @Override public Engine onCreateEngine() {
+    return new ValtoneClockEngine();
+  }
+
+  private class ValtoneClockEngine extends Engine {
+
+    private final Calendar mCalendar = Calendar.getInstance();
+
+    private final Handler mHandler;
+    private Date mDate;
+    private boolean mVisible = true;
+    private int mHalfWidth, mHalfHeight;
+
+    private final Runnable mDrawRunner = new Runnable() {
+      @Override public void run() {
+        draw();
+      }
+    };
+
+    ValtoneClockEngine() {
+      mHandler = new Handler();
+      mDate = new Date();
+
+      final Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/OpenSans-Regular.ttf");
+      PAINT.setTypeface(typeface);
+    }
+
+    @Override public void onVisibilityChanged(boolean visible) {
+      this.mVisible = visible;
+      if (visible) {
+        mHandler.post(mDrawRunner);
+      } else {
+        mHandler.removeCallbacks(mDrawRunner);
+      }
+    }
+
+    @Override public void onSurfaceDestroyed(SurfaceHolder holder) {
+      super.onSurfaceDestroyed(holder);
+      this.mVisible = false;
+      mHandler.removeCallbacks(mDrawRunner);
+    }
+
     @Override
-    public Engine onCreateEngine() {
-        return new ValtoneClockEngine();
+    public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+      this.mHalfWidth = width >> 1;
+      this.mHalfHeight = height >> 1;
+      PAINT.setTextSize(width / 6);
+      super.onSurfaceChanged(holder, format, width, height);
     }
 
-    private class ValtoneClockEngine extends Engine {
-        private final Handler mHandler;
-        private Date mDate;
-        private Paint mPaint;
-        private boolean mVisible = true;
-        private int mWidth, mHeight;
+    private void draw() {
+      final SurfaceHolder holder = getSurfaceHolder();
+      Canvas canvas = null;
+      try {
+        canvas = holder.lockCanvas();
+        mDate = new Date();
 
-        private final Runnable mDrawRunner = new Runnable() {
-            @Override
-            public void run() {
-                draw();
-            }
-        };
+        final String date = DATE_FORMAT.format(mDate);
+        mCalendar.setTime(mDate);
 
-        ValtoneClockEngine() {
-            mHandler = new Handler();
-            mDate = new Date(System.currentTimeMillis());
-            mPaint = new Paint();
-            mPaint.setColor(Color.WHITE);
-            mPaint.setAntiAlias(true);
-            Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/OpenSans-Regular.ttf");
-            mPaint.setTypeface(typeface);
-        }
-        @Override
-        public void onVisibilityChanged(boolean visible) {
-            this.mVisible = visible;
-            if (visible) {
-                mHandler.post(mDrawRunner);
-            } else {
-                mHandler.removeCallbacks(mDrawRunner);
-            }
-        }
-        @Override
-        public void onSurfaceDestroyed(SurfaceHolder holder) {
-            super.onSurfaceDestroyed(holder);
-            this.mVisible = false;
-            mHandler.removeCallbacks(mDrawRunner);
-        }
+        final int hours = mCalendar.get(Calendar.HOUR_OF_DAY);
+        final int minutes = mCalendar.get(Calendar.MINUTE);
+        final int seconds = mCalendar.get(Calendar.SECOND);
 
-        @Override
-        public void onSurfaceChanged(SurfaceHolder holder, int format,
-                                     int width, int height) {
-            this.mWidth = width;
-            this.mHeight = height;
-            mPaint.setTextSize(width/6);
-            super.onSurfaceChanged(holder, format, width, height);
-        }
+        canvas.drawColor(
+            Color.parseColor(String.format(Locale.US, "#%02d%02d%02d", hours, minutes, seconds)));
 
-        private int fastConvertHexToDec(int number) {
-            Log.d("NUMBER", " " + number);
-            return Integer.valueOf(Integer.toString(number), 16);
+        canvas.drawText(date, mHalfWidth - PAINT.measureText(date) / 2, mHalfHeight, PAINT);
+      } finally {
+        if (canvas != null) {
+          holder.unlockCanvasAndPost(canvas);
         }
-
-        private void draw() {
-            SurfaceHolder holder = getSurfaceHolder();
-            Canvas canvas = null;
-            try {
-                canvas = holder.lockCanvas();
-                long timeMillis = System.currentTimeMillis();
-                mDate = new Date(timeMillis);
-                String hh, mm, ss;
-                hh = mDate.getHours()<10?("0"+mDate.getHours()):Integer.toString(mDate.getHours());
-                mm = mDate.getMinutes()<10?("0"+mDate.getMinutes()):Integer.toString(mDate.getMinutes());
-                ss = mDate.getSeconds()<10?("0"+mDate.getSeconds()):Integer.toString(mDate.getSeconds());
-                String date = hh + " : " + mm + " : " + ss;
-                int hours = fastConvertHexToDec(mDate.getHours());
-                int minutes = fastConvertHexToDec(mDate.getMinutes());
-                int seconds = fastConvertHexToDec(mDate.getSeconds());
-                canvas.drawColor(Color.rgb(hours,minutes,seconds));
-                canvas.drawText(date,mWidth/2 - mPaint.measureText(date)/2,mHeight/2,mPaint);
-            } finally {
-                if (canvas != null)
-                    holder.unlockCanvasAndPost(canvas);
-            }
-            mHandler.removeCallbacks(mDrawRunner);
-            if (mVisible) {
-                mHandler.postDelayed(mDrawRunner, 1000);
-            }
-        }
+      }
+      mHandler.removeCallbacks(mDrawRunner);
+      if (mVisible) {
+        mHandler.postDelayed(mDrawRunner, DELAY_MILLIS);
+      }
     }
+  }
 }
